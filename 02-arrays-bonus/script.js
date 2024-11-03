b702bebbf1e063da9f9c41247fd8abe0fe2c2e3c
vincenzobozzanca <vinci.boz@gmail.com>
2024-11-04 00:25:20 +0100

    BONUS

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,10 +10,14 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.reverse();
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter((name) => name.length >= 5);
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+const ed = teachers.indexOf("Ed");
+teachers.splice(5, 1);
+console.log(teachers);
